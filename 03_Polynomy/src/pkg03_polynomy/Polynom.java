9d8fedb40e31eb205e7cbf36bf420bd3b79911ca
RadekMocek <radek.mocek@tul.cz>
2022-03-20 21:34:24 +0100

    Polynom done

--- 03_Polynomy/src/pkg03_polynomy/Polynom.java
+++ 03_Polynomy/src/pkg03_polynomy/Polynom.java
@@ -4,18 +4,25 @@ import java.util.Arrays;
 
 public class Polynom {
     
-    private int[] coeffs;
+    private final int[] coeffs;
+    private final int len;
     
     private Polynom(int[] coeffs) {
-        int len = coeffs.length;
-        while (coeffs[len-1] == 0) {            
-            if (len == 1) throw new IllegalArgumentException("Prázdný polynom");
-            len--;
+        int pointer = coeffs.length;
+        // Remove trailing zeroes
+        while (coeffs[pointer - 1] == 0) {            
+            if (pointer == 1) {
+                this.coeffs = new int[]{0};
+                len = 1;
+                return;
+            }
+            pointer--;
         }        
-        this.coeffs = Arrays.copyOf(coeffs, len);
+        this.coeffs = Arrays.copyOf(coeffs, pointer);
+        len = this.coeffs.length;
     }
     
-    // Human friendly
+    // Tovární metoda – User friendly
     public static Polynom getInstanceDownwardly(int... a) {
         int len = a.length;
         int[] b = new int[len];
@@ -25,7 +32,7 @@ public class Polynom {
         return new Polynom(b);
     }
     
-    // Machine friendly
+    // Tovární metoda – Coder friendly
     public static Polynom getInstanceUpwardly(int... a) {
         return new Polynom(a);
     }
@@ -33,16 +40,19 @@ public class Polynom {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("");        
-        int len = coeffs.length;
         for (int i = len - 1; i >= 0; i--) {
-            sb.append("(").append(coeffs[i]).append(")*x^").append(i);
-            if (i != 0) sb.append(" + ");
+            int coeff = coeffs[i];            
+            if (coeff > 0 && i != len - 1) sb.append("+");
+            sb.append(coeff);
+            if (i != 0) sb.append("*x");
+            if (i > 1) sb.append("^").append(i);
+            sb.append(" ");
         }
         return sb.toString();
     }
     
     public int getDegree() {
-        return (coeffs.length - 1);
+        return (len - 1);
     }
     
     public int getCoefficient(int exponent) {
@@ -50,12 +60,42 @@ public class Polynom {
         return (coeffs[exponent]);
     }
     
+    public int getValue(int x) {        
+        int value = coeffs[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            value *= x;
+            value += coeffs[i];
+        }
+        return value;
+    }
+    
+    public Polynom getDerivate() {
+        int[] derivate = new int[len - 1];
+        for (int i = 1; i < len; i++) {
+            derivate[i - 1] = i * coeffs[i];
+        }
+        return getInstanceUpwardly(derivate);
+    }
+    
     // Debug
     public static void main(String[] args) {        
         
-        Polynom pol = Polynom.getInstanceDownwardly(1,2,3);        
+        /*
+        Polynom pol = Polynom.getInstanceDownwardly(1,-2,3,-4);        
         System.out.println(pol);
         System.out.println("stupeň: " + pol.getDegree());
         System.out.println(pol.getCoefficient(2));
+        System.out.println(pol.getValue(-2));
+        
+        var der = pol.getDerivate();
+        System.out.println(der);
+        */
+        
+        Polynom polA = Polynom.getInstanceDownwardly(1,2,3);
+        Polynom polB = Polynom.getInstanceDownwardly(5,6,7,8);
+        Polynom sum = PolynomTools.Sum(polA, polB);
+        Polynom mul = PolynomTools.Multiply(polA, polB);
+        System.out.println(polA+"\n"+polB+"\n-----");
+        System.out.println(mul);
     }
 }
--- 03_Polynomy/src/pkg03_polynomy/PolynomTools.java
+++ 03_Polynomy/src/pkg03_polynomy/PolynomTools.java
@@ -2,4 +2,36 @@ package pkg03_polynomy;
 
 public class PolynomTools {
     
+    public static Polynom Sum(Polynom a, Polynom b) {        
+        int lenA = a.getDegree() + 1;
+        int lenB = b.getDegree() + 1;
+        
+        int len = Math.max(lenA, lenB);
+        int[] sum = new int[len];
+        
+        for (int i = 0; i < len; i++) {            
+            sum[i] = 0;
+            if (i < lenA) sum[i] += a.getCoefficient(i);
+            if (i < lenB) sum[i] += b.getCoefficient(i);
+        }        
+        
+        return Polynom.getInstanceUpwardly(sum);
+    }
+    
+    public static Polynom Multiply(Polynom a, Polynom b) {
+        int lenA = a.getDegree() + 1;
+        int lenB = b.getDegree() + 1;
+        
+        int len = lenA + lenB - 1;
+        int[] mul = new int[len];
+        for (int i = 0; i < len; i++) mul[i] = 0;
+        
+        for (int i = 0; i < lenA; i++) {
+            for (int j = 0; j < lenB; j++) {
+                mul[i + j] += a.getCoefficient(i) * b.getCoefficient(j);
+            }
+        }
+        
+        return Polynom.getInstanceUpwardly(mul);
+    }
 }
