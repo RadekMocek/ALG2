127560896dfe3d42d58000ddf04c5dc70da01db2
RadekMocek <radek.mocek@tul.cz>
2022-05-11 15:56:47 +0200

    Filé moment II

--- 06_Banka/src/pkg06_banka/Transakce.java
+++ 06_Banka/src/pkg06_banka/Transakce.java
@@ -1,10 +1,16 @@
 package pkg06_banka;
 
+import java.io.File;
+import java.time.LocalDate;
+
 public class Transakce {
 
+    /*
     private int year;
     private int month;
     private int day;
+    */
+    private LocalDate date;
     private int sum;
 
     public Transakce(/*int year, int month, int day,*/ int sum) {
@@ -13,7 +19,13 @@ public class Transakce {
         this.month = month;
         this.day = day;
         */
+        this.date = LocalDate.now();
         this.sum = sum;
+    }
+
+    void saveToFile(File file) {
+        long nDays = date.toEpochDay();
+        //try w/ resources dataoutputstream 😣
     }
 
 }
